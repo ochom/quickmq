b8a4e39b6e8fe4f020c0523ad6d3b24775ea3c02
ochom <ochomrichard752@gmail.com>
2023-03-21 16:31:18 +0300

    fix consumer

--- clients/consumer.go
+++ clients/consumer.go
@@ -6,24 +6,17 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// WorkerFunc is a function that processes a message
-type WorkerFunc func([]byte) error
-
 // Consumer is a struct that holds the name of the queue and the items in the queue
 type Consumer struct {
-	host       string
-	queueName  string
-	workerFunc WorkerFunc
-	workers    int
+	host      string
+	queueName string
 }
 
 // NewConsumer creates a new Consumer
-func NewConsumer(url string, queueName string, workerFunc WorkerFunc, workers int) *Consumer {
+func NewConsumer(url string, queueName string) *Consumer {
 	return &Consumer{
-		host:       url,
-		queueName:  queueName,
-		workerFunc: workerFunc,
-		workers:    workers,
+		host:      url,
+		queueName: queueName,
 	}
 }
 
@@ -32,56 +25,30 @@ func (c *Consumer) GetQueueName() string {
 	return c.queueName
 }
 
-// GetWorkers returns the number of workers
-func (c *Consumer) GetWorkers() int {
-	return c.workers
-}
-
 // Consume consumes a message from a queue
-func (c *Consumer) Consume() error {
-	msgs := c.getMessages()
-
-	for msg := range msgs {
-		if err := c.workerFunc(msg); err != nil {
-			c.reQueue(msg)
-		}
-	}
-
-	return nil
-}
-
-// getMessages consumes a message from a queue
-func (c *Consumer) getMessages() <-chan []byte {
-	cl, _, err := websocket.DefaultDialer.Dial(c.host+"/consume?queue="+c.queueName, nil)
+func (c *Consumer) Consume(workerFunc func([]byte)) error {
+	cl, res, err := websocket.DefaultDialer.Dial(c.host+"/consume?queue="+c.queueName, nil)
 	if err != nil {
 		log.Println("Error dialing: ", err.Error())
 		return nil
 	}
 
-	deliveries := make(chan []byte)
-	go func() {
+	if res.StatusCode != 101 {
+		log.Println("Error dialing: code:", res.StatusCode, res.Status)
+		return nil
+	}
 
-		for {
-			_, data, err := cl.ReadMessage()
-			if err != nil {
-				log.Println("Error reading message: ", err.Error())
-				break
-			}
+	defer cl.Close()
 
-			deliveries <- data
+	for {
+		_, data, err := cl.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message: ", err.Error())
+			break
 		}
 
-		close(deliveries)
-	}()
-
-	return deliveries
-}
-
-// reQueue re-queues a message
-func (c *Consumer) reQueue(data []byte) {
-	p := NewPublisher(c.host, c.queueName)
-	if err := p.Publish(data); err != nil {
-		log.Println("Error requeuing message: ", err.Error())
-		return
+		workerFunc(data)
 	}
+
+	return nil
 }
