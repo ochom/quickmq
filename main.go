a13621391440832bd57fe581653db86cd67e479e
ochom <ochomrichard752@gmail.com>
2023-03-22 14:26:53 +0300

    refactor code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 	"os/signal"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/ochom/quickmq/dto"
 	"github.com/ochom/quickmq/models"
 )
 
@@ -79,4 +81,86 @@ func main() {
 
 	go cron(mq, repo, stop)
 	<-stop
+}
+
+func publish(mq *quickMQ, repo *models.Repo) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		defer func() {
+			if err := ws.Close(); err != nil {
+				log.Println("Error closing websocket: ", err.Error())
+			}
+		}()
+
+		_, data, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message: ", err.Error())
+			return
+		}
+
+		req := &dto.PublishRequest{}
+		if err := json.Unmarshal(data, req); err != nil {
+			log.Println("Error un-marshaling: ", err.Error())
+			return
+		}
+
+		if req.Delay == 0 {
+			item := models.NewItem(req.Queue, req.Data, req.Delay)
+			mq.publish(item)
+			return
+		}
+
+		// if delay is greater than cron time, add queue and items to database
+		item := models.NewItem(req.Queue, req.Data, req.Delay)
+		if err := repo.SaveItem(item); err != nil {
+			log.Println("Error adding to repo: ", err.Error())
+		}
+	}
+}
+
+func consume(mq *quickMQ) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		queueName := c.Query("queue")
+		if queueName == "" {
+			c.JSON(400, gin.H{"error": "queue is required"})
+			return
+		}
+
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		closed := make(chan bool, 1)
+
+		// consume messages from queue and write to websocket but stop when websocket is closed
+		go func() {
+			for {
+				select {
+				case msg := <-mq.consume(queueName):
+					if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil {
+						log.Println("Error writing to websocket: ", err.Error())
+						return
+					}
+				case <-closed:
+					return
+				}
+			}
+		}()
+
+		// wait for websocket to close
+		for {
+			_, _, err := ws.ReadMessage()
+			if err != nil {
+				closed <- true
+				break
+			}
+		}
+	}
 }
