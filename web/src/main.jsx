fdc4139a1b9d14e71fe000e7e9405dbf155d56eb
ochom <ochomrichard752@gmail.com>
2024-06-13 18:47:42 +0300

    uopdate build

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Session from './Session.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Session>
-      <App />
-    </Session>
-  </React.StrictMode>
+  <Session>
+    <App />
+  </Session>
 );
